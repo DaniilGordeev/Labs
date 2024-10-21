9d3d9c976e557e1995940f26df42552f84fa6361
Daniil <daniil.gordeew231@gmail.com>
2024-10-21 17:41:22 +0300

    Edited Program

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program 
+{
+    static void Main(string[] args)
+    {
+        bool continueProgram = true;
+        Console.WriteLine("Программу сделал Гордеев Даниил");
+        while (continueProgram)
+        {
+            try
+            {
+                Console.Write("Введите номер программы: ");
+                int key = int.Parse(Console.ReadLine()!);
+
+                switch (key)
+                {
+                    case 0:
+                        continueProgram = false;
+                        break;
+
+                    // case 3:
+                    //     Lab3.Task3(args);
+                    //     break;
+
+                    // case 4:
+                    //     Lab4.Task4(args);
+                    //     break;
+
+                    // case 5:
+                    //     Lab5.Task5(args);
+                    //     break;
+
+                    // case 6:
+                    //     Lab6.Task6(args);
+                    //     break;
+
+                    default:
+                        Console.WriteLine("Такой программы нет...");
+                        break;
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+
+        }
+    }
+}
