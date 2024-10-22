1572d39307b669268a08db83dfa89c3f50fa06ed
Daniil <daniil.gordeew231@gmail.com>
2024-10-22 14:34:15 +0300

    lab 2

---
+++ Labs/lab3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Point
+{
+    private int x;
+    private int y;
+
+    public Point() { }
+
+    public void SetX(int value)
+    {
+        if (value < 0)
+        {
+            throw new Exception("Координата X не может быть отрицательной.");
+        }
+        x = value;
+    }
+
+    public void SetY(int value)
+    {
+        if (value < 0)
+        {
+            throw new Exception("Координата Y не может быть отрицательной.");
+        }
+        y = value;
+    }
+
+    public void Draw(int x, int y)
+    {
+        SetX(x);
+        SetY(y);
+
+        Console.SetCursorPosition(this.x, this.y);
+        Console.Write("()()");
+        Console.SetCursorPosition(this.x, this.y + 1);
+        Console.Write(" /\\ ");
+    }
+}
+
+class Lab3
+{
+    public static void Task3(string[] args)
+    {
+        try
+        {
+            Point point = new Point();
+            point.Draw(3, 3); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
